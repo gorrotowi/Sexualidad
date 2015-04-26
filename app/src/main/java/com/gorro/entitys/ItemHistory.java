85c66ff4c11bdc807943b4ffe2b62bd142917255
Gorro <t_o_wi@gmail.com>
2015-04-26 17:37:25 -0500

    + Agregando item de historia

---
+++ app/src/main/java/com/gorro/entitys/ItemHistory.java
@@ -0,0 +1,21 @@
+package com.gorro.entitys;
+
+/**
+ * Created by gorro on 26/04/15.
+ */
+public class ItemHistory {
+
+    String history;
+
+    public ItemHistory(String history) {
+        this.history = history;
+    }
+
+    public String getHistory() {
+        return history;
+    }
+
+    public void setHistory(String history) {
+        this.history = history;
+    }
+}
