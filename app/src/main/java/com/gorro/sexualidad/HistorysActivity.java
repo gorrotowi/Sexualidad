d03a6570845bc5ee33f9f8dacc5f5e274ad39f0b
Gorro <t_o_wi@gmail.com>
2015-04-27 02:29:11 -0500

    + agregando header y dialogo de historia nueva

--- app/src/main/java/com/gorro/sexualidad/HistorysActivity.java
+++ app/src/main/java/com/gorro/sexualidad/HistorysActivity.java
@@ -3,10 +3,15 @@ package com.gorro.sexualidad;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AbsListView;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.gorro.adapters.AdapterHistory;
 import com.gorro.entitys.ItemHistory;
 
@@ -26,15 +31,11 @@ public class HistorysActivity extends ActionBarActivity {
 
         listHistorys = (ListView) findViewById(R.id.listHistorys);
         toolbar = (Toolbar) findViewById(R.id.toolbarHistorys);
+        toolbar.setBackground(getResources().getDrawable(R.drawable.historys));
         setSupportActionBar(toolbar);
+//        getSupportActionBar().setHideOnContentScrollEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        adapter = new AdapterHistory(HistorysActivity.this, R.layout.item_history, getData());
-        listHistorys.setAdapter(adapter);
     }
 
     private ArrayList<ItemHistory> getData() {
@@ -61,13 +62,42 @@ public class HistorysActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (item.getItemId() == R.id.action_add) {
+            newHistory();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void newHistory() {
+        new MaterialDialog.Builder(HistorysActivity.this)
+                .title(R.string.writeYourHistory)
+                .customView(R.layout.history_dialog, true)
+                .positiveText("Enviar")
+                .negativeText("Cancelar")
+                .callback(new MaterialDialog.ButtonCallback() {
+                    @Override
+                    public void onPositive(MaterialDialog dialog) {
+                        super.onPositive(dialog);
+                        String history = ((EditText) dialog.findViewById(R.id.edtxHistoryDialog)).getText().toString();
+                        if (history.length() > 10) {
+                            sendHistory(history);
+                        } else {
+                            Toast.makeText(HistorysActivity.this, "Tu historia es muy corta", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onNegative(MaterialDialog dialog) {
+                        super.onNegative(dialog);
+                        dialog.dismiss();
+                    }
+                })
+                .show();
+    }
+
+    private void sendHistory(String history) {
+        Toast.makeText(HistorysActivity.this, "Tu historia ha sido enviada", Toast.LENGTH_SHORT).show();
     }
 }
